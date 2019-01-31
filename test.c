c05a5630914d646ca5066c621f4795d39a429274
opensynergy-polarion <47206960+opensynergy-polarion@users.noreply.github.com>
2019-01-31 08:44:22 +0100

    Create test.c

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* {@wi.implements Prototype_03/PROT3-1201 This is an implementation of test case serial-fifo.serial_fifo_init.serial_fifo_init }
+*/ 
+int test_serial_fifo_init() {
+  return 0;
+}
+
+/**
+* {@wi.implements Prototype_03/PROT3-1202 This is an implementation of test case serial-fifo.serial_fifo_init.serial_fifo_init }
+*/ 
+int test_serial_fifo_init_2() {
+  return 0;
+}
